1622ca2cf10e043c03703a294bf7c9109742702e
Emilio Gutter <egutter@10pines.com>
2012-08-22 21:07:56 -0300

    Allowed to override styles by param

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ end
 
 gem 'jquery-rails'
 gem 'less-rails-bootstrap'
+gem 'nokogiri'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+require 'open-uri'
+require 'nokogiri'
+
 module ApplicationHelper
+
+  def render_styles_from_url
+    if params[:styles_url].present?
+      Nokogiri::HTML(open(params[:styles_url])).at_xpath('//textarea').text.html_safe
+    end
+  end
 end
