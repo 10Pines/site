a18e936a2bb7bb3e3fb7b81e1534f4a8d6c7cd00
Emilio Gutter <egutter@10pines.com>
2012-09-03 22:35:19 -0300

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,20 @@
-require 'open-uri'
-require 'nokogiri'
+#require 'open-uri'
+#require 'nokogiri'
 require "execjs"
 
 module ApplicationHelper
 
-  def render_styles_from_url
-    if params[:styles_url].present?
-      Nokogiri::HTML(open(params[:styles_url])).at_xpath('//textarea').text.html_safe
-    end
-  end
+  #def render_styles_from_url
+  #  if params[:styles_url].present?
+  #    Nokogiri::HTML(open(params[:styles_url])).at_xpath('//textarea').text.html_safe
+  #  end
+  #end
 
   def password_strength
-    #source = open("#{Rails.root}/app/assets/javascripts/frequency_lists.js").read
-    #source << open("#{Rails.root}/app/assets/javascripts/adjacency_graphs.js").read
-    #source << open("#{Rails.root}/app/assets/javascripts/scoring.js").read
-    #source << open("#{Rails.root}/app/assets/javascripts/matching.js").read
-    #source << open("#{Rails.root}/app/assets/javascripts/init.js").read
     source = File.open("#{Rails.root}/config/zxcvbn.js").read
 
     puts ExecJS.runtime.name
+    puts source
     context = ExecJS.compile(source)
     result = context.call("zxcvbn", 'pepe')
     result['score']
