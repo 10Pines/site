49b7e70052f4b822282980c07c97f39033805bd5
Emilio Gutter <egutter@10pines.com>
2012-09-19 10:22:06 -0300

    Removed password test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,12 @@
-#require 'open-uri'
-#require 'nokogiri'
-require "execjs"
+require 'open-uri'
+require 'nokogiri'
 
 module ApplicationHelper
 
-  #def render_styles_from_url
-  #  if params[:styles_url].present?
-  #    Nokogiri::HTML(open(params[:styles_url])).at_xpath('//textarea').text.html_safe
-  #  end
-  #end
-
-  def password_strength
-    source = File.open("#{Rails.root}/config/zxcvbn.js").read
-
-    puts ExecJS.runtime.name
-    puts source
-    context = ExecJS.compile(source)
-    result = context.call("zxcvbn", 'pepe')
-    result['score']
+  def render_styles_from_url
+   if params[:styles_url].present?
+     Nokogiri::HTML(open(params[:styles_url])).at_xpath('//textarea').text.html_safe
+   end
   end
+
 end
